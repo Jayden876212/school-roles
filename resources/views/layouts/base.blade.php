c54989d9ee4d91a3945c9915561cef4ed05f62dc
Jayden Waghorn <dxmqkgax@duck.com>
2025-03-23 21:30:11 +0000

    Add base layout template

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield("title")</title>
+
+    @include("shared.stylesheets")
+    @yield("styles")
+</head>
+<body>
+    <x-header-component/>
+    <main>
+        @yield("content")
+    </main>
+    <x-footer-component/>
+
+    @include("shared.scripts")
+    @yield("scripts")
+</body>
+</html>
