b1fba35088d6db2ffe9105a90da9041ecbfbbda2
Jayden Waghorn <dxmqkgax@duck.com>
2025-03-30 23:10:26 +0100

    Use non-integer primary key in Grade eloquent model

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -8,4 +8,9 @@ class Grade extends Model
 {
     protected $table = "grades";
     public $timestamps = false;
+
+    // Use non-integer primary string for readability and simplicity (varchar)
+    protected $primaryKey = "grade";
+    public $incrementing = false;
+    protected $keyType = "string";
 }
